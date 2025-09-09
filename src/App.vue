2ace56646f5771f9064c8e66d01dde22f70a9ba7
siixo <nicolas.knittel@proton.me>
2025-09-09 16:07:32 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+</script>
+
+<template>
+  <div class="h-screen w-screen flex flex-col">
+    <Navbar />
+    <main class="flex flex-col flex-1 items-center justify-center bg-gray-800">
+      <router-view />
+    </main>
+    <Footer />
+  </div>
+</template>
